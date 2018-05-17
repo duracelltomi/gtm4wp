30490f7543dbe04c36c5c122cecb9046f76e3afd
Vincent K <koconder@gmail.com>
2018-05-17 17:29:07 +1000

    Injection method improvement
    Adding additional injection checks and improvements

--- integration/amp.php
+++ integration/amp.php
@@ -36,11 +36,18 @@ function gtm4wp_amp_gtmvariables(){
 // Check AMP Analytics has not been included already
 // https://github.com/Automattic/amp-wp/blob/develop/includes/amp-post-template-actions.php
 function gtm4wp_amp_gtmampcode_check($data){
-	if ( ! empty( $data['amp_analytics'] ) && !$gtm4wp_options[ GTM4WP_OPTION_INTEGRATE_AMPCODE_AMP] ) {
+	if ( ! empty( $data['amp_analytics'] ) ) {
 		$data['amp_component_scripts']['amp-analytics'] = 'https://cdn.ampproject.org/v0/amp-analytics-0.1.js';
 		define('GTM4WP_PLACEMENT_AMP_TAG', true);
+	}else{
+		add_action( 'amp_post_template_head', 'gtm4wp_amp_gtmampcode_head');
 	}
 	return $data;
+}
+
+function gtm4wp_amp_gtmampcode_head(){
+	echo '<script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>';
+	define('GTM4WP_PLACEMENT_AMP_TAG', true);
 }
 
 // Some injection for AMP code
