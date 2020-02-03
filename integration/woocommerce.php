2aadf7c444cd34986126dd0fcb05e6b5a0ccde11
Thomas Geiger <duracelltomi@users.noreply.github.com>
2020-02-03 16:59:05 +0100

    Do not try to add order data into data layer if $order variable was unset before

--- integration/woocommerce.php
+++ integration/woocommerce.php
@@ -425,7 +425,7 @@ function gtm4wp_woocommerce_datalayer_filter_items( $dataLayer ) {
 			}
 		}
 
-		if ( $gtm4wp_options[ GTM4WP_OPTION_INTEGRATE_WCORDERDATA ] && $gtm4wp_is_woocommerce3 ) {
+		if ( isset($order) && $gtm4wp_options[ GTM4WP_OPTION_INTEGRATE_WCORDERDATA ] && $gtm4wp_is_woocommerce3 ) {
 			$order_items = gtm4wp_process_order_items( $order );
 
 			$dataLayer['orderData'] = array(
