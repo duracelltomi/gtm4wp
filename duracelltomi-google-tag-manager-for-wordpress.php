c1bef66cff020f37d751ac48c6b5fa3efca99e3d
Thomas Geiger <duracelltomi@users.noreply.github.com>
2021-05-13 21:07:52 +0200

    Updated WordPress and WooCommerce compatibility and stable version number

--- duracelltomi-google-tag-manager-for-wordpress.php
+++ duracelltomi-google-tag-manager-for-wordpress.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Google Tag Manager for Wordpress
-Version: 1.12.2
+Version: 1.13
 Plugin URI: https://gtm4wp.com/
 Description: The first Google Tag Manager plugin for WordPress with business goals in mind
 Author: Thomas Geiger
@@ -10,10 +10,10 @@ Text Domain: duracelltomi-google-tag-manager
 Domain Path: /languages
 
 WC requires at least: 3.2
-WC tested up to: 5.1.0
+WC tested up to: 5.3.0
 */
 
-define( 'GTM4WP_VERSION',    '1.12.2' );
+define( 'GTM4WP_VERSION',    '1.13' );
 define( 'GTM4WP_PATH',       plugin_dir_path( __FILE__ ) );
 
 global $gtp4wp_plugin_url, $gtp4wp_plugin_basename;
