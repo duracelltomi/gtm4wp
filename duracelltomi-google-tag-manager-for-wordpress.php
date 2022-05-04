4907ffe276e6d2490a8116af46fd361908f20153
Thomas Geiger <duracelltomi@users.noreply.github.com>
2022-05-04 16:18:31 +0200

    Updated plugin version number + corrected all phpcs suggestions

--- duracelltomi-google-tag-manager-for-wordpress.php
+++ duracelltomi-google-tag-manager-for-wordpress.php
@@ -1,26 +1,45 @@
 <?php
-/*
-Plugin Name: Google Tag Manager for Wordpress
-Version: 1.15
-Plugin URI: https://gtm4wp.com/
-Description: The first Google Tag Manager plugin for WordPress with business goals in mind
-Author: Thomas Geiger
-Author URI: https://gtm4wp.com/
-Text Domain: duracelltomi-google-tag-manager
-Domain Path: /languages
+/**
+ * Google Tag Manager for WordPress
+ *
+ * @package GTM4WP
+ * @author Thomas Geiger
+ * @copyright 2013- Geiger Tamás e.v. (Thomas Geiger s.e.)
+ * @license GNU General Public License, version 3
+ *
+ * @wordpress-plugin
+ * Plugin Name: Google Tag Manager for WordPress
+ * Plugin URI: https://gtm4wp.com/
+ * Description: The first Google Tag Manager plugin for WordPress with business goals in mind
+ * Version: 1.15.1
+ * Requires at least: 3.4.0
+ * Requires PHP: 5.6
+ * Author: Thomas Geiger
+ * Author URI: https://gtm4wp.com/
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: duracelltomi-google-tag-manager
+ * Domain Path: /languages
 
-WC requires at least: 3.2
-WC tested up to: 6.3.1
-*/
+ * WC requires at least: 3.2
+ * WC tested up to: 6.3.1
+ */
 
-define( 'GTM4WP_VERSION',    '1.15' );
-define( 'GTM4WP_PATH',       plugin_dir_path( __FILE__ ) );
+define( 'GTM4WP_VERSION', '1.15.1' );
+define( 'GTM4WP_PATH', plugin_dir_path( __FILE__ ) );
 
 global $gtp4wp_plugin_url, $gtp4wp_plugin_basename;
 $gtp4wp_plugin_url      = plugin_dir_url( __FILE__ );
 $gtp4wp_plugin_basename = plugin_basename( __FILE__ );
 require_once GTM4WP_PATH . '/common/readoptions.php';
 
+/**
+ * WordPress hook function run after plugins have been loaded.
+ *
+ * @see https://developer.wordpress.org/reference/hooks/plugins_loaded/
+ *
+ * @return void
+ */
 function gtm4wp_init() {
 	load_plugin_textdomain( 'duracelltomi-google-tag-manager', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	if ( is_admin() ) {
