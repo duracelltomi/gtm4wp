6f435520a562ba70ec1a5c22c4edff7a54cffcdf
Thomas Geiger <duracelltomi@users.noreply.github.com>
2022-05-12 17:16:11 +0200

    Added PHPDoc

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,15 @@
 <?php
-// if uninstall.php is not called by WordPress, die
+/**
+ * Loaded when GTM4WP was uninstalled.
+ * Deletes plugin options from database.
+ *
+ * @package GTM4WP
+ * @author Thomas Geiger
+ * @copyright 2013- Geiger Tamás e.v. (Thomas Geiger s.e.)
+ * @license GNU General Public License, version 3
+ */
+
+// if uninstall.php is not called by WordPress, die.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
