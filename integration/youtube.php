32fec878a0fe2687836d04e533aaadd6220d3045
Thomas Geiger <duracelltomi@users.noreply.github.com>
2022-05-30 16:32:14 +0200

    Updates using PHPCS report
    - proper escaping even if overprotective
    - proper code formatting

--- integration/youtube.php
+++ integration/youtube.php
@@ -1,7 +1,27 @@
 <?php
+/**
+ * Vimeo integration related codes.
+ * Enabled JS API in YouTube embed codes and loads the interaction tracking script of GTM4WP.
+ *
+ * @package GTM4WP
+ * @author Thomas Geiger
+ * @copyright 2013- Geiger Tamás e.v. (Thomas Geiger s.e.)
+ * @license GNU General Public License, version 3
+ */
+
+/**
+ * Adds loading of the JS API of the YouTube player into the embed codes.
+ *
+ * @see https://developer.wordpress.org/reference/hooks/oembed_result/
+ *
+ * @param string|false $return The returned oEmbed HTML (false if unsafe).
+ * @param string       $url URL of the content to be embedded.
+ * @param string|array $data Additional arguments for retrieving embed HTML. See wp_oembed_get() for accepted arguments. Default empty.
+ * @return string
+ */
 function gtm4wp_youtube( $return, $url, $data ) {
 	$site_url       = site_url();
-	$site_url_parts = parse_url( $site_url );
+	$site_url_parts = wp_parse_url( $site_url );
 
 	if ( false !== strpos( $return, 'youtube.com' ) ) {
 		return str_replace( 'feature=oembed', 'feature=oembed&enablejsapi=1&origin=' . $site_url_parts['scheme'] . '://' . $site_url_parts['host'], $return );
