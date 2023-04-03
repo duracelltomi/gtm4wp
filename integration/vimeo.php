cd6bc74d43320d3f9578467a5186e2f5966889ba
Robin Schülein <robins@animalequality.de>
2023-04-03 15:41:14 +0200

    add gulp to transpile frontend with babel and minify afterwards

--- integration/vimeo.php
+++ integration/vimeo.php
@@ -13,5 +13,5 @@ if ( ! is_admin() ) {
 	$in_footer = (bool) apply_filters( 'gtm4wp_vimeo', true );
 
 	wp_enqueue_script( 'gtm4wp-vimeo-api', 'https://player.vimeo.com/api/player.js', array(), '1.0', $in_footer );
-	wp_enqueue_script( 'gtm4wp-vimeo', $gtp4wp_plugin_url . 'js/gtm4wp-vimeo.js', array(), GTM4WP_VERSION, $in_footer );
+	wp_enqueue_script( 'gtm4wp-vimeo', $gtp4wp_script_path . 'gtm4wp-vimeo.js', array(), GTM4WP_VERSION, $in_footer );
 }
