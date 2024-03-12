2c5db7b76531feb5a8e43148089780a0c45b7fcb
Thomas Geiger <duracelltomi@gmail.com>
2024-03-12 14:41:09 +0100

    Updated version numbers

--- duracelltomi-google-tag-manager-for-wordpress.php
+++ duracelltomi-google-tag-manager-for-wordpress.php
@@ -11,7 +11,7 @@
  * Plugin Name: GTM4WP - A Google Tag Manager (GTM) plugin for WordPress
  * Plugin URI: https://gtm4wp.com/
  * Description: The first Google Tag Manager plugin for WordPress with business goals in mind
- * Version: 1.20
+ * Version: 1.20.1
  * Requires at least: 3.4.0
  * Requires PHP: 7.4
  * Author: Thomas Geiger
@@ -22,10 +22,10 @@
  * Domain Path: /languages
 
  * WC requires at least: 5.0
- * WC tested up to: 8.6
+ * WC tested up to: 8.7
  */
 
-define( 'GTM4WP_VERSION', '1.20' );
+define( 'GTM4WP_VERSION', '1.20.1' );
 define( 'GTM4WP_PATH', plugin_dir_path( __FILE__ ) );
 
 global $gtp4wp_plugin_url, $gtp4wp_plugin_basename, $gtp4wp_script_path;
